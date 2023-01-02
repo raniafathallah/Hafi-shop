65bfc1d0b7ec25a2711dd798ebe4c3155a2c9fcd
raniafathallah <raniaahmed7000@gmail.com>
2023-01-02 22:31:02 +0200

    addReviewsToProduct

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -119,6 +119,42 @@ const updateProduct = catchAsync(async (req, res) => {
 // @routes POST /api/products/:id/reviews
 // @access Private
 const createReview = catchAsync(async (req, res) => {
+     const { rating, comment } = req.body;
+
+     const product = await Product.findById(req.params.id);
+   
+     if (product) {
+       const alreadyReviewed = product.reviews.find(
+         (r) => r.user.toString() === req.user._id.toString()
+       );
+   
+       if (alreadyReviewed) {
+         res.status(400);
+         throw new Error('you had reviewed before ');
+       }
+   
+       const review = {
+         name: req.user.name,
+         rating: Number(rating),
+         comment,
+         user: req.user._id,
+       };
+   
+       product.reviews.push(review);
+   
+       product.numReviews = product.reviews.length;
+   
+       product.rating =
+         product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+         product.reviews.length;
+   
+       await product.save();
+       res.status(201).json({ message: 'Review added' });
+     } else {
+       res.status(404);
+       throw new Error('Product not found');
+     }
+
 
 
   
