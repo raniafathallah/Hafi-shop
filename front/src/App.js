be7862bdf90707e0ef2942b4cdec83f7176261de
raniafathallah <raniaahmed7000@gmail.com>
2023-01-10 02:41:20 +0200

    addImagesSlider

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import {Menu} from './components/Menu'
+import {Menu} from './components/Menu';
+import {CarouselD} from './components/Carousel';
 function App() {
   return (
     <>
     <div className="App">
       <Menu/>
+      <div className='carousel-container'>
+         <CarouselD/>
+
+      </div>
 
     </div>
     </>
